83953a5cdfe8f696b7bafbf0089a5f747509546d
Alyssa <alyssam.anderson@yahoo.com>
2024-05-01 01:57:43 -0700

    finished

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import kaboom from "kaboom"
+import kaboom from "kaboom";
 
-const k = kaboom();
+//initialize context
 
-k.loadSprite("bean", "www\sprites\bean.png");
+kaboom();
+
+//load assest
+loadSprite("bird","www/sprites/bird.png");
+loadSprite("bg","www/sprites/bg.png");
+loadSprite("bpipe","www/sprites/pipe.png");
 
 k.add([
 	k.pos(120, 80),
@@ -11,23 +16,25 @@ k.add([
 
 k.onClick(() => k.addKaboom(k.mousePos()));
 
-//load sounds
-const jumpSound = loadSound("jump", "www/sounds/jump.mp3");
-const bruhSound = loadSound("bruh", "www/sounds/bruh.mp3");
-const passSound = loadSound("pass", "www/sounds/pass.mp3");
+// Load sounds
+loadSound("jump", "www/sounds/jump.mp3");
+loadSound("bruh", "www/sounds/bruh.mp3");
+loadSound("pass", "www/sounds/pass.mp3");
 
 let highScore = 0;
 
-//Game Scene
-screen("game", () => {
+// Game Scene
+scene("game", () => {
 	const PIPE_GAP = 140;
 	let score = 0;
 	setGravity(1600);
 
-	add([sprite("bg", { width: width(), height: height() })]);
-
+	add([
+		sprite("bg", { width: width(), height: height() })
+	]);
 
 	const scoreText = add([text(score), pos(12, 12)]);
+	
 	const player = add([
 		sprite("bird"),
 		scale(1.2),
@@ -43,18 +50,18 @@ screen("game", () => {
 			sprite("pipe"),
 			pos(width(), height() / 2 + offset + PIPE_GAP / 2),
 			"pipe",
-			scale(2)
+			scale(2), 
 			area(),
 			{ passed: false },
 		]);
 
 		//top pipe
 		add([
-			sprite("pipe", { flipY: true }),
+			sprite("www/sprites/pipe.png", { flipY: true }),
 			pos(width(), height() / 2 + offset + PIPE_GAP / 2),
 			"pipe",
 			anchor("botleft"),
-			scale(2)
+			scale(2),
 			area()
 		]);
 
@@ -120,3 +127,132 @@ scene("gameover", (score, screenshot) => {
 
 //Start the game
 go("game");
+
+
+
+
+
+/*import kaboom from "kaboom";
+
+//initialize context
+
+kaboom();
+
+//load assest
+loadSprite("bird","sprites/bird.png");
+loadSprite("bg","sprites/bg.png");
+loadSprite("pipe","sprites/pipe.png");
+
+// Load sounds
+loadSound("jump", "sounds/jump.mp3");
+loadSound("bruh", "sounds/bruh.mp3");
+loadSound("pass", "sounds/pass.mp3");
+
+let highScore = 0;
+
+// Game Scene
+scene("game", () => {
+    const PIPE_GAP = 140;
+    let score = 0;
+    setGravity(1600);
+
+    add([
+        sprite("bg", { width: width(), height: height() })
+    ]);
+
+    const scoreText = add([text(score), pos(12, 12)]);
+
+    const player = add([
+        sprite("bird"),
+        scale(1.2),
+        pos(100, 50),
+        area(),
+        body(),
+    ]);
+    function createPipes() {
+        const offset = rand(-50, 50);
+
+        //bottom pipe
+        add([
+            sprite("pipe"),
+            pos(width(), height() / 2 + offset + PIPE_GAP / 2),
+            "pipe",
+            scale(2),
+            area(),
+            { passed: false },
+        ]);
+
+        //top pipe
+        add([
+            sprite("pipe", { flipY: true }),
+            pos(width(), height() / 2 + offset - PIPE_GAP / 2),
+            "pipe",
+            anchor("botleft"),
+            scale(2),
+            area()
+        ]);
+
+    }
+
+    loop(1.5, () => createPipes());
+
+    onUpdate("pipe", (pipe) => {
+        pipe.move(-300, 0);
+
+        if (pipe.passed === false && pipe.pos.x < player.pos.x) {
+            pipe.passed = true;
+            score += 1;
+            scoreText.text = score;
+            play("pass");
+        }
+    });
+
+    player.onCollide("pipe", () => {
+        const ss = screenshot();
+        go("gameover", score, ss);
+    });
+
+    player.onUpdate(() => {
+        if (player.pos.y > height()) {
+            const ss = screenshot();
+            go("gameover", score, ss);
+        }
+    });
+
+    onKeyPress("space", () => {
+        play("jump");
+        player.jump(400);
+    });
+
+    //For touch
+    window.addEventListener("touchstart", () => {
+        play("jump");
+        player.jump(400);
+    });
+
+});
+
+//Game over scene
+scene("gameover", (score, screenshot) => {
+    if (score > highScore) highScore = score;
+
+    play("bruh");
+
+    loadSprite("gameOverScreen", screenshot);
+    add([sprite("gameOverScreen", { width: width(), height: height() })]);
+
+    add([
+        text("gameover!\n" + "score: " + score + "\nhigh score: " + highScore, { size: 45 }),
+        pos(width() / 2, height() / 3),
+    ]);
+
+	onKeyPress("space", () => {
+		go("game");
+
+    })
+
+});
+
+//Start the game
+go("game");
+*/
